10bb949b20cae398962586b3d773e9696fe5dbd8
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-10-04 17:58:44 +0530

    fix: versions

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { config } from "@gluestack-ui/config";
+import { config } from "./../gluestack-ui.config";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 
 export function Providers({ children }: { children: React.ReactNode }) {
